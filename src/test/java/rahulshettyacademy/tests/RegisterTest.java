d15ca5c481c9aacedd9e396a844afaeb67ec08df
Rose <rosedaniel.s7528@gmail.com>
2024-11-29 18:27:42 +0530

    v.02

--- src/test/java/rahulshettyacademy/tests/RegisterTest.java
+++ src/test/java/rahulshettyacademy/tests/RegisterTest.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class RegisterTest {
 	
-	static WebDriver driver;
+	static WebDriver driver; //cicd demo
 	
 
 	public static void main(String[] args) {
